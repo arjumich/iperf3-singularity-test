52478a73231eb876574a8e4bd6d311bc89ad1b60
arjumich <arjunsaha2010@gmail.com>
2024-02-27 00:52:14 +0000

    first commit to upload to github

---
+++ iperf3-automation-test/iperf3-tests.sh
@@ -0,0 +1,63 @@
+#!/bin/bash 
+# IP address of the iperf server
+IP_ADDRESS="172.31.43.131"
+
+# Number of iperf parallel client connections
+NUM_CONNECTIONS_LIST=(1 4 8 12 50
+)
+
+#for connection numbers 1, grep '\[  4\]' | awk '{print $7}' | tail -n 2 | head -n 1 | bc)
+
+
+# Duration of the iperf test (in seconds)
+TEST_DURATION=10
+
+echo "Running iperf3 tests for 10 seconds on each pass with single and multiple stream"
+echo "Test starting...."
+
+
+#loop over single/parallel connections in range
+for NUM_CONNECTIONS in "${NUM_CONNECTIONS_LIST[@]}" ; do
+
+        echo "Running test for $NUM_CONNECTIONS stream: "
+        total_bandwidth=0
+
+#run 3 times for one setting and take average of 3 passes
+        for ((i = 1; i <= 3; i++)); do
+                # Run iperf test
+                iperf_result=$(iperf3 -c "$IP_ADDRESS" -P "$NUM_CONNECTIONS" -t "$TEST_DURATION" -f m)
+#               echo "$iperf_result"
+
+
+                # Extract bandwidth from iperf result
+                #for connection numbers 1, grep '\[  4\]' | awk '{print $7}' | tail -n 2 | head -n 1 | bc)
+                if [ $NUM_CONNECTIONS -eq 1 ]; then
+                        sender_bandwidth=$(echo "$iperf_result" | grep '\[  5\]' | awk '{print $7}' | tail -n 2 | head -n 1 | bc)
+#                       echo "$sender_bandwidth"
+#                       sender_bandwidth2=$(echo $"sender_bandwidth/1" | bc)
+#                       echo "$sender_bandwidth2"
+                else
+                        sender_bandwidth=$(echo "$iperf_result" | grep SUM | awk '{print $6}' | tail -n 2 | head -n 1 | bc)
+#                       sender_bandwidth=$(echo $"sender_bandwidth/1" | bc)
+                fi
+
+                echo "Bandwidth rate on $i Pass: $sender_bandwidth Mbits/sec"
+
+#               total_bandwidth=$((total_bandwidth + sender_bandwidth))
+                total_bandwidth=$((total_bandwidth + $(echo "$sender_bandwidth / 1" | bc)))
+#               echo "$total_bandwidth"
+
+#		if [i -eq 1] || [i -eq 2];then 
+		echo "Taking 1 minute brake before next pass..."
+		sleep 1
+#		fi
+        done
+
+        average=$((total_bandwidth/3))
+
+        echo "Average Bandwidth for $NUM_CONNECTIONS stream after 3 passes: $average Mbits/sec"
+        echo " "
+
+	echo "Taking 5 minute brake before next Pass"
+	sleep 1
+done
---
+++ iperf3-automation-test/modify_tcp_buffer_size.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+   	echo "Usage: 1: View current buffer size"
+	echo "Usage: 2: Set max buffer size (8MB)"
+	echo "Usage: 1: Reset to default buffer size"
+    exit 1
+fi
+
+choice="$1"
+
+case $choice in
+    1)
+        sysctl net.core.rmem_max
+	sysctl net.core.wmem_max
+	sysctl net.ipv4.tcp_rmem
+	sysctl net.ipv4.tcp_wmem
+	;;
+    2)
+        sysctl -w net.core.rmem_max=8388608
+	sysctl -w net.core.wmem_max=8388608
+	sysctl -w net.ipv4.tcp_rmem='8388608 8388608 8388608'
+	sysctl -w net.ipv4.tcp_wmem='8388608 8388608 8388608'
+	sysctl -w net.ipv4.route.flush=1
+        ;;
+    3)
+        sysctl -w net.core.rmem_max=212992
+        sysctl -w net.core.wmem_max=212992
+        sysctl -w net.ipv4.tcp_rmem='4096 131072 5129504'
+        sysctl -w net.ipv4.tcp_wmem='4096 16384 4194304'
+	sysctl -w net.ipv4.route.flush=1
+        ;;
+
+esac
