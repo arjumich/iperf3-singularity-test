869ca23a66c9dbfcfbdd40014a8a19677dfc8fd8
arjumich <arjunsaha2010@gmail.com>
2024-03-04 19:50:31 +0000

    latest update

--- iperf3-automation-test/auto_test.sh
+++ iperf3-automation-test/auto_test.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+DELAY_LIST=(0 50 75 100 200
+)
 
+OUTPUT_FOLDER=automation_test
 
+mkdir $OUTPUT_FOLDER
 
-./iperf3-tests.sh | tee win_scaling_0_outputs/0ms_0.1_loss_no_mod_output.txt
+for DELAY in "${DELAY_LIST[@]}" ; do
+
+	echo "Generating test results for default and modified buffer for "$DELAY"ms delay and 0.1% loss... "
+	echo " "
+
+	echo "Default Buffer: "
+	./emmulate_wan.sh -d eth0
+	./emmulate_wan.sh eth0 "$DELAY" 0.1
+	./modify_tcp_buffer_size.sh 3
+	./iperf3-tests.sh | tee $OUTPUT_FOLDER/"$DELAY"ms_0.1_loss_no_mod_output.txt
+
+	echo "Modified Buffer: "
+        ./modify_tcp_buffer_size.sh 2
+        ./iperf3-tests.sh | tee $OUTPUT_FOLDER/"$DELAY"ms_0.1_loss_mod_output.txt
+
+done
