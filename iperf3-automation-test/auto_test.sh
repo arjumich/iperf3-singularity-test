b30af0f7b690af9f2112e104c07011f6268ff2bc
arjumich <arjunsaha2010@gmail.com>
2024-03-05 12:31:47 -0500

    results of HPC_BUF_autotune_testrun_1 uploaded and some minor tweaks

--- iperf3-automation-test/auto_test.sh
+++ iperf3-automation-test/auto_test.sh
@@ -3,7 +3,7 @@
 DELAY_LIST=(0 25 50 75 100 200
 )
 
-OUTPUT_FOLDER=HPC_Server_testrun_1
+OUTPUT_FOLDER=HPC_BUF_autotune_testrun_1
 
 mkdir $OUTPUT_FOLDER
 
@@ -18,14 +18,14 @@ for DELAY in "${DELAY_LIST[@]}" ; do
 	echo "--------------------------------"
 	./emmulate_wan.sh enp0s3 "$DELAY" 0.1
 	echo "------------------------------- "
-	./modify_tcp_buffer_size.sh 3
+	./calc_modify_tcp_buffer_size.sh 3
 	echo "------------------------------- "
 	./iperf3-tests.sh | tee $OUTPUT_FOLDER/"$DELAY"ms_0.1_loss_no_mod_output.txt
 
 
 	echo "Modified Buffer: "
 	echo "---------------------------------------------------------------------------- "
-        ./modify_tcp_buffer_size.sh 2
+        ./calc_modify_tcp_buffer_size.sh 2 $DELAY
         ./iperf3-tests.sh | tee $OUTPUT_FOLDER/"$DELAY"ms_0.1_loss_mod_output.txt
 
 done
